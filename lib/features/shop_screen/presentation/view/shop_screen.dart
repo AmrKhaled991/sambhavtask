36efa93b6fb1d563cc50bc4e44b61235691f9af0
Amr Kaled <amrkhaled033@gmail.com>
2025-04-21 12:51:03 +0200

    handel all categories

--- lib/features/shop_screen/presentation/view/shop_screen.dart
+++ lib/features/shop_screen/presentation/view/shop_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sambhavtask/constant.dart';
-import 'package:sambhavtask/core/utils/theme/appAssets.dart';
 import 'package:sambhavtask/features/shop_screen/presentation/view/widgets/Second_image_slider_with_indicator.dart';
 import 'package:sambhavtask/features/shop_screen/presentation/view/widgets/brands_section.dart';
 import 'package:sambhavtask/features/shop_screen/presentation/view/widgets/horizontal_product_list.dart';
@@ -8,14 +6,10 @@ import 'package:sambhavtask/features/shop_screen/presentation/view/widgets/image
 import 'package:sambhavtask/features/shop_screen/presentation/view/widgets/most_popular_product.dart';
 import 'package:sambhavtask/features/shop_screen/presentation/view/widgets/high_lights_section.dart';
 import 'package:sambhavtask/features/shop_screen/presentation/view/widgets/popular_stores_section.dart';
-import 'package:sambhavtask/features/shop_screen/presentation/view/widgets/product_card.dart';
 import 'package:sambhavtask/features/shop_screen/presentation/view/widgets/shope_location_and_notifications.dart';
 import 'package:sambhavtask/features/shop_screen/presentation/view/widgets/special_offer.dart';
 import 'package:sambhavtask/features/shop_screen/presentation/view/widgets/top_offers_near_me.dart';
-import 'package:sambhavtask/features/shop_screen/presentation/view/widgets/top_offers_near_me_item.dart';
-import 'package:sambhavtask/features/widgets/custom_fav_button.dart';
 import 'package:sambhavtask/features/widgets/search_header_delegate.dart';
-import 'package:sambhavtask/features/widgets/section_see_all_header.dart';
 
 class ShopScreen extends StatefulWidget {
   const ShopScreen({super.key});
@@ -56,7 +50,7 @@ class _ShopScreenState extends State<ShopScreen> {
             child: Column(
               children: [
                 FirstImageSliderWithIndicator(),
-                HorizontalProductList(),
+                HorizontalCategoriesList(),
                 MostPopularProduct(),
                 SecondImageSliderWithIndicator(),
                 SizedBox(height: 20),
